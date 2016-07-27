fe15da04204e2272a22b4313613887b7e3a15093
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-27 10:28:16 -0400

    MVP. Added fully functional create, update, and destroy options.

--- app.rb
+++ app.rb
@@ -3,30 +3,51 @@ require "sinatra"
 require "sinatra/reloader"
 require "pg"
 require "active_record"
-require "pry"
+# require "pry"
 
 require_relative "db/connection"
 require_relative "models/pokemon"
 
-binding.pry
+# binding.pry
 
-puts "end of app"
-
+# puts "end of app"
 
 get '/pokemons' do
   @pokemons = Pokemon.all
   erb :"pokemons/index"
 end
 
-get 'pokemons/new' do
+get '/pokemons/new' do
   erb :"pokemons/new"
-end
-
-get '/' do
-  redirect "/pokemons"
 end
 
 get '/pokemons/:id' do
   @pokemons = Pokemon.find(params[:id])
   erb :"pokemons/show"
+end
+
+post '/pokemons' do
+  pokemons = Pokemon.create(params[:pokemon])
+  redirect "/pokemons/#{pokemons.id}"
+end
+
+get '/pokemons/:id/edit' do
+  @pokemons = Pokemon.find(params[:id])
+  erb :"pokemons/edit"
+end
+
+put '/pokemons/:id' do
+  @pokemons = Pokemon.find(params[:id])
+  @pokemons.update(params[:pokemon])
+  redirect "/pokemons/#{@pokemons.id}"
+end
+
+delete '/pokemons/:id' do
+  @pokemons = Pokemon.find(params[:id])
+  @pokemons.destroy
+  redirect "/pokemons"
+end
+
+get '/' do
+  redirect "/pokemons"
 end
